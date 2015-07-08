d097a8905086acb5c42c880d3cc7baf93757b114
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-07-08 16:46:52 +0900

    Chef/Berkshelf use current process environment

--- operation/chef.go
+++ operation/chef.go
@@ -282,7 +282,9 @@ func (o *ChefOperation) defaultConfig() (map[string]interface{}, error) {
 func (o *ChefOperation) executeBerkshelf() error {
 	cmd := exec.Command("berks", "vendor", "cookbooks")
 	cmd.Dir = o.patternDir()
-	cmd.Env = []string{"HOME=/root"}
+	env := os.Environ()
+	env = append(env, "HOME=/root")
+	cmd.Env = env
 	out, err := cmd.CombinedOutput()
 	fmt.Println(string(out))
 
@@ -306,7 +308,9 @@ func (o *ChefOperation) executeChef(conf string, json string) error {
 	fmt.Printf("Execute chef(conf: %s, json: %s)\n", conf, json)
 	cmd := exec.Command("chef-solo", "-c", conf, "-j", json)
 	cmd.Dir = o.patternDir()
-	cmd.Env = []string{"HOME=/root"}
+	env := os.Environ()
+	env = append(env, "HOME=/root")
+	cmd.Env = env
 	out, err := cmd.CombinedOutput()
 	fmt.Println(string(out))
 	fmt.Println(err)
