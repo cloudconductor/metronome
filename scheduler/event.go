989c2b3b7ac6f1cec01da311e0bdbee306b073ee
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-08-27 09:29:05 +0900

    Set default priority on event with 50

--- scheduler/event.go
+++ scheduler/event.go
@@ -1,6 +1,10 @@
 package scheduler
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"metronome/util"
+)
 
 type Event struct {
 	Path         string
@@ -13,6 +17,22 @@ type Event struct {
 }
 
 type Events []Event
+
+func (e *Event) UnmarshalJSON(d []byte) error {
+	e.Priority = 50
+
+	m := make(map[string]json.RawMessage)
+	u := &util.UnmarshalContext{}
+	u.Unmarshal(d, &m)
+	u.Unmarshal([]byte(m["path"]), &e.Path)
+	u.Unmarshal([]byte(m["pattern"]), &e.Pattern)
+	u.Unmarshal([]byte(m["name"]), &e.Name)
+	u.Unmarshal([]byte(m["description"]), &e.Description)
+	u.Unmarshal([]byte(m["priority"]), &e.Priority)
+	u.Unmarshal([]byte(m["ordered_tasks"]), &e.OrderedTasks)
+	u.Unmarshal([]byte(m["task"]), &e.Task)
+	return u.Err
+}
 
 func (e *Event) SetPattern(path string, pattern string) {
 	e.Path = path
